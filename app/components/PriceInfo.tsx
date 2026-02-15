6b77a6b4246763fe97b6214a7e8b75dd2ae170e0
“jeeva-sparkouttech” <jeevanantham@sparkouttech.com>
2026-02-15 22:20:27 +0530

    ref : total supply value fetch interval changed

--- app/components/PriceInfo.tsx
+++ app/components/PriceInfo.tsx
@@ -54,7 +54,7 @@ export default function PriceInfo() {
     functionName: 'totalSupply',
     query: {
       enabled: isConnected && !!contractAddress,
-      refetchInterval: 40000,
+      refetchInterval: 30000,
     }
   });
 
